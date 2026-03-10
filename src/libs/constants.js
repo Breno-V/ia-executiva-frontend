5a94e5482a3f8198fc5e29df07099cdfbfaad1c4
Breno Valentim <breno.valentim@texantech.com.br>
2026-03-10 09:06:34 -0300

    feat - criando variaveis constantes

--- src/libs/constants.js
+++ src/libs/constants.js
@@ -1,1 +1,8 @@
 // arquivo com valores fixos do projeto que são usados em vários lugares
+
+export const COLORS = {
+  accent: "#2EB7D9",
+  alertHigh: "#e46868",
+  alertMedium: "#e6ac6e",
+  alertLow: "#94d578",
+};
