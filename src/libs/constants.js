d50535f4da19b334c48a2bef4700ecd24ff6ce89
Breno Valentim <breno.valentim@texantech.com.br>
2026-03-17 10:11:11 -0300

    feat - Adição de mais valores constantes

--- src/libs/constants.js
+++ src/libs/constants.js
@@ -6,3 +6,11 @@ export const COLORS = {
   alertMedium: "#e6ac6e",
   alertLow: "#94d578",
 };
+
+export const REGION_COORDS = {
+  "Sudeste": { lat: -23.55, lng: -46.63, city: "São Paulo" },
+  "Sul":     { lat: -25.42, lng: -49.27, city: "Curitiba" },
+  "Nordeste": { lat: -12.97, lng: -38.50, city: "Salvador" },
+  "Norte":   { lat: -3.10,  lng: -60.02, city: "Manaus" },
+  "Centro-Oeste": { lat: -15.77, lng: -47.92, city: "Brasília" },
+};
